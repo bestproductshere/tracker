53c762fc206960f83852b5e1a8d6aa8686988a22
bestproductshere <inspiredollar@gmail.com>
2025-08-30 22:59:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 window.TRACKER_SETTINGS = {
-  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia0bf-20&linkId=b125944fcbc4c3251aee8c3d2cd0e613&language=en_US&ref_=as_li_ss_tl",
+  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=pt-bestproductshere-20&linkId=4cbf7f049f00583d7866a400afa89e48&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 0.1,
   webhook_url: "https://golden-ideally-skunk.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",

eed2b0cdefe758a228e91515c68e56e4a87448ea
bestproductshere <inspiredollar@gmail.com>
2025-09-24 17:05:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 window.TRACKER_SETTINGS = {
   redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=pt-bestproductshere1-20&linkId=3bcd422606574bb395be81fc13540080&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 0.1,
-  webhook_url: "https://golden-ideally-skunk.ngrok-free.app/webhook/PT-Tracker",
+  webhook_url: "https://exoticwhollylionfish.brainchain.shop/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
   button_text: "View on Amazon",
   disclaimer: "Commission may be earned from the links below with no extra cost to you."

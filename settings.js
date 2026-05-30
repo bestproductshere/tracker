f8c942cdf288f5da1704e1cd497bfdbc11233b19
bestproductshere <inspiredollar@gmail.com>
2026-05-30 13:42:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 window.TRACKER_SETTINGS = {
-  redirect_url: "https://linktwin.co/TKdLuR",
+  redirect_url: "https://linktwin.co/ZtzFRk",
   redirect_delay: 0.001,
   webhook_url: "https://exoticwhollylionfish.brainchain.shop/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
